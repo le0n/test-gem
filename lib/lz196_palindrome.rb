78a1115145f99b6136279dcf61db2514b4c29e3f
Leon Zheng <leon.zheng@hotmail.com>
2023-06-26 15:43:14 +1000

    initialize repo

---
+++ lib/lz196_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lz196_palindrome/version"
+
+module Lz196Palindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lz196_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Lz196Palindrome
+  VERSION = "0.1.0"
+end
